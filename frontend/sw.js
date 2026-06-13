c185780956ef1d5bf81605b2acc76a07b3f49ad8
LaibaMaqbool <44s4c65krv@privaterelay.appleid.com>
2026-06-13 13:48:56 +0500

    Create sw.js

---
+++ frontend/sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'bytechat-v1';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(['/index.html']))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() =>
+      caches.match(e.request)
+    )
+  );
+});
